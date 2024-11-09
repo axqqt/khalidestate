055dd60d66cc4e5e77fb90c58f797241eae55018
Veloxal <127298642+axqqt@users.noreply.github.com>
2024-11-09 18:21:52 +0530

    estate

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
-import Hero from '@/components/home/Hero';
-import Services from '@/components/home/Services';
-import Contact from '@/components/home/Contact';
+"use client"
+import Contact from "@/componets/home/Contact";
+import Hero from "@/componets/home/Hero";
+import Services from "@/componets/home/Services";
 
 export default function Home() {
   return (
